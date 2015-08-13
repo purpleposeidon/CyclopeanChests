bc021eb3116f662724c625f5cfeb65f4a81c9bef
neptunepink <purpleposeidon@gmail.com>
2015-08-13 03:16:28 -0700

    Wrestling with ASM in obf environ...

--- src/main/java/io/github/purpleposeidon/oldchests/asm/TransformChests.java
+++ src/main/java/io/github/purpleposeidon/oldchests/asm/TransformChests.java
@@ -18,32 +18,41 @@ public class TransformChests implements IClassTransformer {
         ClassNode cn = new ClassNode();
         cr.accept(cn, 0);
 
+        int changed = 0;
         for (MethodNode method : cn.methods) {
-            if (!method.name.equals("registerBlocks")) continue;
+            if (!method.name.equals("registerBlocks") && !method.name.equals("func_149671_p") && !(method.name.equals("p") && method.desc.equals("()V"))) continue;
             ListIterator<AbstractInsnNode> it = method.instructions.iterator();
             while (it.hasNext()) {
                 AbstractInsnNode insn = it.next();
                 final String chest_orig = "net/minecraft/block/BlockChest";
+                final String chest_notch = "ajx";
                 final String chest_repl = "io/github/purpleposeidon/oldchests/ChestReplacement";
+                final String ender_notch = "akv";
                 final String ender_orig = "net/minecraft/block/BlockEnderChest";
                 final String ender_repl = "io/github/purpleposeidon/oldchests/EnderChestReplacement";
                 if (insn.getType() == AbstractInsnNode.TYPE_INSN) {
                     TypeInsnNode type = (TypeInsnNode) insn;
-                    if (type.desc.equals(chest_orig)) {
+                    if (type.desc.equals(chest_orig) || type.desc.equals(chest_notch)) {
                         type.desc = chest_repl;
-                    } else if (type.desc.equals(ender_orig)) {
+                        changed++;
+                    } else if (type.desc.equals(ender_orig) || type.desc.equals(ender_notch)) {
                         type.desc = ender_repl;
+                        changed++;
                     }
                 } else if (insn.getType() == AbstractInsnNode.METHOD_INSN) {
                     MethodInsnNode call = (MethodInsnNode) insn;
-                    if (call.owner.equals(chest_orig)) {
+                    if (call.owner.equals(chest_orig) || call.owner.equals(chest_notch)) {
                         call.owner = chest_repl;
-                    } else if (call.owner.equals(ender_orig)) {
+                    } else if (call.owner.equals(ender_orig) || call.owner.equals(ender_notch)) {
                         call.owner = ender_repl;
                     }
                 }
             }
             break;
+        }
+
+        if (changed != 3) {
+            throw new RuntimeException("Replacements failed! Expected 3, got: " + changed);
         }
 
         int flags = 0; // FIXME: Troubles running with intellij? Different JVM or something?
